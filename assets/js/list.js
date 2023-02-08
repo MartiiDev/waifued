3870f44e5e04fab2b81f34cb021fae8639f19942
Edgar C <44440955+MartiiDev@users.noreply.github.com>
2023-02-08 19:22:24 +0100

    Add files via upload

---
+++ assets/js/list.js
@@ -0,0 +1,83 @@
+var LIST = [
+	{
+	  	name: "Napoléon Bonaparte",
+	  	subtitle: "First Emperor of the French",
+	    from: {
+    		title: "Napoléon Maiden",
+    		type: "game",
+    		url: "https://store.steampowered.com/app/1504270/"
+	    }
+	},
+	{
+	  	name: "Horatio Nelson",
+	  	subtitle: "British vice-amiral",
+	    from: {
+    		title: "Napoléon Maiden",
+    		type: "game",
+    		url: "https://store.steampowered.com/app/1504270/"
+	    }
+	},
+	{
+	  	name: "Charles-Henri Sanson",
+	  	subtitle: "French royal executioner",
+	    from: {
+    		title: "Napoléon Maiden",
+    		type: "game",
+    		url: "https://store.steampowered.com/app/1504270/"
+	    }
+	},
+	{
+	  	name: "Adolf Hitler",
+	  	subtitle: "Dictator of Nazi Germany",
+	    from: {
+    		title: "Mein Waifu is the Fuhrer",
+    		type: "game",
+    		url: "https://www.kickstarter.com/projects/1930242464/mein-waifu-is-the-fuhrer-a-parody-visual-novel"
+	    }
+	},
+	{
+	  	name: "Rudolf Hess",
+	  	subtitle: "Deputy Führer in Nazi Germany",
+	    from: {
+    		title: "Mein Waifu is the Fuhrer",
+    		type: "game",
+    		url: "https://www.kickstarter.com/projects/1930242464/mein-waifu-is-the-fuhrer-a-parody-visual-novel"
+	    }
+	},
+	{
+	  	name: "Joseph Goebbels",
+	  	subtitle: "Nazi propaganda minister",
+	    from: {
+    		title: "Mein Waifu is the Fuhrer",
+    		type: "game",
+    		url: "https://www.kickstarter.com/projects/1930242464/mein-waifu-is-the-fuhrer-a-parody-visual-novel"
+	    }
+	},
+	{
+	  	name: "Heinrich Himmler",
+	  	subtitle: "Leading member of the Nazi Party",
+	    from: {
+    		title: "Mein Waifu is the Fuhrer",
+    		type: "game",
+    		url: "https://www.kickstarter.com/projects/1930242464/mein-waifu-is-the-fuhrer-a-parody-visual-novel"
+	    }
+	},
+	{
+	  	name: "Hermann Goering",
+	  	subtitle: "Nazi military leader",
+	    from: {
+    		title: "Mein Waifu is the Fuhrer",
+    		type: "game",
+    		url: "https://www.kickstarter.com/projects/1930242464/mein-waifu-is-the-fuhrer-a-parody-visual-novel"
+	    }
+	},
+	{
+	  	name: "Isaac Newton",
+	  	subtitle: "English mathematician",
+	    from: {
+    		title: "Newton to Ringo no Ki",
+    		type: "game",
+    		url: "https://vndb.org/v20330"
+	    }
+	},
+]
